9ca568b43d1eb4ce1a262d6f0a539eda0c4a6d13
서재학 <vulcanus1981@gmail.com>
2023-01-16 23:37:33 +0900

    test

--- page_view/lib/main.dart
+++ page_view/lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp( //test
       home: HomeScreen(),
     );
   }
