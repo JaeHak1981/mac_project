e34943ac53c5d9f8874757fe50d5d6dee7990b2e
서재학 <vulcanus1981@gmail.com>
2023-02-05 20:48:16 +0900

    2023/02/05 WebView update

--- web_view/lib/page/HomeScreen.dart
+++ web_view/lib/page/HomeScreen.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-final uri = Uri.parse(
+final homeUrl = Uri.parse(
     'https://bubbly-cause-fb4.notion.site/Class-c3284153a2074e9fa8423c193cba40bf');
 
 class HomeScreen extends StatelessWidget {
   WebViewController controller = WebViewController()
     ..setJavaScriptMode(JavaScriptMode.unrestricted)
-    ..loadRequest(uri);
+    ..loadRequest(homeUrl);
 
   HomeScreen({Key? key}) : super(key: key);
 
@@ -18,12 +18,14 @@ class HomeScreen extends StatelessWidget {
         actions: [
           IconButton(
               onPressed: () {
-                controller.loadRequest(uri);
+                controller.loadRequest(homeUrl);
               },
               icon: Icon(Icons.home))
         ],
       ),
-      body: WebViewWidget(controller: controller),
+      body: WebViewWidget(
+        controller: controller,
+      ),
     );
   }
 }
