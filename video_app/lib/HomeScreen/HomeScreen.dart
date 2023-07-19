3b0b5ace3551df61f824b4ce833199f6903b09e0
서재학 <104539151+JaeHak1981@users.noreply.github.com>
2023-07-19 20:36:25 +0900

    2023/07/19 changeUpdate

--- video_app/lib/HomeScreen/HomeScreen.dart
+++ video_app/lib/HomeScreen/HomeScreen.dart
@@ -18,29 +18,8 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Container(
         width: MediaQuery.of(context).size.width,
         decoration: getBoxDecoration(),
-        child: video == null ? renderVideoEmpty() : renderVide(),
+        child: video == null ? renderVideoEmpty() : renderVideo(),
       ),
-    );
-  }
-
-  Widget renderVide() {
-    return Center(
-      child: CustomVideoPlayer(
-        video: video!,
-      ),
-    );
-  }
-
-  Widget renderVideoEmpty() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        _Logo(
-          onTap: onNewVideo,
-        ),
-        SizedBox(height: 30),
-        _AppName(),
-      ],
     );
   }
 
@@ -51,8 +30,32 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
+  Widget renderVideo() {
+    return Center(
+      child: CustomVideoPlayer(
+        video: video!,
+        onNewVideo: onNewVideo,
+      ),
+    );
+  }
+
+  Widget renderVideoEmpty() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _Logo(
+          onPressed: onNewVideo,
+        ),
+        SizedBox(
+          height: 30,
+        ),
+        _AppName(),
+      ],
+    );
+  }
+
   BoxDecoration getBoxDecoration() {
-    return const BoxDecoration(
+    return BoxDecoration(
         gradient: LinearGradient(
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
@@ -64,14 +67,14 @@ class _HomeScreenState extends State<HomeScreen> {
 }
 
 class _Logo extends StatelessWidget {
-  final VoidCallback onTap;
+  final VoidCallback onPressed;
 
-  const _Logo({required this.onTap, super.key});
+  const _Logo({required this.onPressed, super.key});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-        onTap: onTap, child: Image.asset('asset/img/logo.png'));
+        onTap: onPressed, child: Image.asset('asset/img/logo.png'));
   }
 }
 
@@ -82,19 +85,20 @@ class _AppName extends StatelessWidget {
   Widget build(BuildContext context) {
     TextStyle textStyle = TextStyle(
         color: Colors.white, fontSize: 30, fontWeight: FontWeight.w300);
-
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text(
-          'VIDEO',
-          style: textStyle,
-        ),
-        Text(
-          'PLAYER',
-          style: textStyle.copyWith(fontWeight: FontWeight.w700),
-        ),
-      ],
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'VIDEO',
+            style: textStyle,
+          ),
+          Text(
+            'PLAYER',
+            style: textStyle.copyWith(fontWeight: FontWeight.w700),
+          ),
+        ],
+      ),
     );
   }
 }
--- video_app/lib/component/CustomVideoPlayer.dart
+++ video_app/lib/component/CustomVideoPlayer.dart
@@ -6,8 +6,10 @@ import 'package:video_player/video_player.dart';
 
 class CustomVideoPlayer extends StatefulWidget {
   final XFile video;
+  final VoidCallback onNewVideo;
 
-  const CustomVideoPlayer({required this.video, super.key});
+  const CustomVideoPlayer(
+      {required this.video, required this.onNewVideo, super.key});
 
   @override
   State<CustomVideoPlayer> createState() => _CustomVideoPlayerState();
@@ -15,6 +17,8 @@ class CustomVideoPlayer extends StatefulWidget {
 
 class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
   VideoPlayerController? videoController;
+  Duration currentPosition = Duration();
+  bool showControls = false;
 
   @override
   void initState() {
@@ -23,32 +27,67 @@ class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
     initializeController();
   }
 
+  @override
+  void didUpdateWidget(covariant CustomVideoPlayer oldWidget) {
+    // TODO: implement didUpdateWidget
+    super.didUpdateWidget(oldWidget);
+
+    if(oldWidget.video.path != widget.video.path){
+      initializeController();
+    }
+  }
+
   void initializeController() async {
+    currentPosition = Duration();
     videoController = VideoPlayerController.file(File(widget.video.path));
     await videoController!.initialize();
+    videoController!.addListener(() {
+      final currentPosition = videoController!.value.position;
+      setState(() {
+        this.currentPosition = currentPosition;
+      });
+    });
     setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     if (videoController == null) {
-      return Center(
+      return const Center(
         child: CircularProgressIndicator(),
       );
     }
     return AspectRatio(
         aspectRatio: videoController!.value.aspectRatio,
-        child: Stack(
-          children: [
-            VideoPlayer(videoController!),
-            _Controls(
-              onPlayPressed: onPlayPressed,
-              onReversePressed: onReversePressed,
-              onForwardPressed: onForwardPressed,
-              isPlaying: videoController!.value.isPlaying,
-            ),
-          ],
+        child: GestureDetector(
+          onTap: () {
+            setState(() {
+              showControls = !showControls;
+            });
+          },
+          child: Stack(
+            children: [
+              VideoPlayer(videoController!),
+              if (showControls)
+                _Controls(
+                  onPlayPressed: onPlayPressed,
+                  onReversePressed: onReversePressed,
+                  onForwardPressed: onForwardPressed,
+                  isPlaying: videoController!.value.isPlaying,
+                ),
+              if (showControls) _NewVideo(onPressed: widget.onNewVideo),
+              _SliderBottom(
+                currentPosition: currentPosition,
+                maxPosition: videoController!.value.duration,
+                onsliderChanged: onSliderChanged,
+              )
+            ],
+          ),
         ));
+  }
+
+  void onSliderChanged(double val) {
+    videoController!.seekTo(Duration(seconds: val.toInt()));
   }
 
   void onPlayPressed() {
@@ -63,11 +102,9 @@ class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
 
   void onReversePressed() {
     final currentPosition = videoController!.value.position;
-
     Duration position = Duration();
-
     if (currentPosition.inSeconds > 3) {
-      position = currentPosition - const Duration(seconds: 3);
+      position = currentPosition - Duration(seconds: 3);
     }
     videoController!.seekTo(position);
   }
@@ -75,12 +112,10 @@ class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
   void onForwardPressed() {
     final maxPosition = videoController!.value.duration;
     final currentPosition = videoController!.value.position;
-
     Duration position = maxPosition;
-
-    if ((maxPosition - const Duration(seconds: 3)).inSeconds >
+    if ((maxPosition - Duration(seconds: 3)).inSeconds >
         currentPosition.inSeconds) {
-      position = currentPosition + const Duration(seconds: 3);
+      position = currentPosition + Duration(seconds: 3);
     }
     videoController!.seekTo(position);
   }
@@ -102,9 +137,9 @@ class _Controls extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      height: MediaQuery.of(context).size.height,
       color: Colors.black.withOpacity(0.5),
       child: Row(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           renderIconButton(
@@ -127,8 +162,71 @@ class _Controls extends StatelessWidget {
         onPressed: onPressed,
         icon: Icon(
           iconData,
-          color: Colors.white,
           size: 30,
+          color: Colors.white,
         ));
+  }
+}
+
+class _NewVideo extends StatelessWidget {
+  final VoidCallback onPressed;
+
+  const _NewVideo({required this.onPressed, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      right: 0,
+      child: IconButton(
+          onPressed: onPressed,
+          icon: Icon(
+            Icons.photo_camera_back,
+            color: Colors.white,
+            size: 30,
+          )),
+    );
+  }
+}
+
+class _SliderBottom extends StatelessWidget {
+  final Duration currentPosition;
+  final Duration maxPosition;
+  final ValueChanged<double> onsliderChanged;
+
+  const _SliderBottom(
+      {required this.currentPosition,
+      required this.maxPosition,
+      required this.onsliderChanged,
+      super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      right: 0,
+      left: 0,
+      bottom: 0,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8),
+        child: Row(
+          children: [
+            Text(
+              '${currentPosition.inMinutes}:${(currentPosition.inSeconds % 60).toString().padLeft(2, '0')}',
+              style: TextStyle(color: Colors.white),
+            ),
+            Expanded(
+              child: Slider(
+                  min: 0,
+                  max: maxPosition.inSeconds.toDouble(),
+                  value: currentPosition.inSeconds.toDouble(),
+                  onChanged: onsliderChanged),
+            ),
+            Text(
+              '${maxPosition.inMinutes}:${(maxPosition.inSeconds % 60).toString().padLeft(2, '0')}',
+              style: TextStyle(color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
