6ca3882f56907c3ad3ffea7d877405e0cab33be0
서재학 <vulcanus1981@gmail.com>
2024-01-03 13:30:07 +0900

    1/3 dartPractice

--- dart_practice/bin/dart_practice.dart
+++ dart_practice/bin/dart_practice.dart
@@ -1,14 +1,1 @@
-void main() {
-
-  Point aPoint=Point(11, 22);
-  print('${aPoint.x}, ${aPoint.y}');
-  print('se');
-
-}
-
-class Point {
-  int x;
-  int? y;
-
-  Point(this.x, this.y ?? 0);
-}
+void main(){}
