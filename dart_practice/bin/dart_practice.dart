1c49a47acc33d5b1976b8774e3488c1a20439b75
서재학 <vulcanus1981@gmail.com>
2024-01-03 15:28:08 +0900

    update

--- dart_practice/bin/dart_practice.dart
+++ dart_practice/bin/dart_practice.dart
@@ -1,1 +1,12 @@
-void main(){}
+import 'dart:io';
+
+void main() {
+  var inputName ='';
+  stdout.write('이름을 입력하세요 : ');
+  inputName = stdin.readLineSync()!;
+  String myName;
+  myName = inputName;
+  print(myName);
+
+
+}
